87383b51386a63e6d2fb140551cf43606df59c4a
KESHAR MEHRA <keshar.mehra@gmail.com>
2017-08-14 19:44:52 +0530

    add connect the disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
-const path = require('path')
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io')
+
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -8,8 +11,21 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port,() => {
+io.on('connection', (socket) => {
+    console.log('new user connected...'); 
+socket.on('disconnect', () => {
+    console.log('user disconnected...');
+});
+
+});
+
+
+
+server.listen(port,() => {
     console.log("server started on localhost at port 3000");
-})
+});
